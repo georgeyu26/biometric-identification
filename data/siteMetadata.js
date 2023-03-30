ca9910bbf6b1584bdfc0c07b8037d5587aef571c
Simeng He <28720612+simenghe@users.noreply.github.com>
2023-03-30 16:49:26 -0400

    Start of nigeria

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'CS492 Final Project',
+  author: 'Simeng He',
+  headerTitle: 'CS492 project',
+  description: 'A CS492 blog + website created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
