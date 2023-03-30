78455eadea91f023c312cbd769c88b403efd253a
George Yu <georgeyu26@gmail.com>
2023-03-30 15:50:12 -0400

    Add image upload to UI

--- pages/face.js
+++ pages/face.js
@@ -2,9 +2,27 @@ import axios from 'axios'
 import React, { useState } from 'react'
 
 export default function FacePage() {
+  const [imageFile, setImageFile] = useState(null)
+
+  async function handleDemo() {
+    try {
+      const url = '/api/face'
+      const resp = await axios.get(url)
+      console.log(resp.data)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  function handleFileChange(e) {
+    setImageFile(event.target.files[0])
+  }
+
   async function handleSubmit(e) {
     e.preventDefault()
+    console.log(imageFile)
     const formData = new FormData()
+    formData.append('imageFile', imageFile)
 
     try {
       const resp = await axios.post('/api/face', formData)
@@ -13,13 +31,21 @@ export default function FacePage() {
       console.error(err)
     }
   }
+
   return (
     <>
       <div>Test for face recognition</div>
       <div></div>
       <form onSubmit={handleSubmit}>
+        <div>
+          <label>Upload an image file here!</label>
+        </div>
+        <input type="file" value={imageFile} accept="image/*" />
         <button type="submit">Submit</button>
       </form>
+      <button className="mt-4 rounded-sm outline" onClick={handleDemo}>
+        Handle Demo Button
+      </button>
     </>
   )
 }
