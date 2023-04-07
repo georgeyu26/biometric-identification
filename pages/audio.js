50578e18417c5dc377ef2f32044ac5296c911a91
Keefe Ho <keefeho@Keefes-MacBook-Pro-2.local>
2023-04-07 16:51:18 -0400

    audio stuff

--- pages/audio.js
+++ pages/audio.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 
+const WHISPER_API_KEY = 'XJNKRP9CD14F2Z5Q4ZCJ3ZUKFJM48YYA'
+
 export default function WhisperPage() {
   const [audioFile, setAudioFile] = useState(null)
 
@@ -15,18 +17,23 @@ export default function WhisperPage() {
   }
 
   function handleFileChange(e) {
-    setAudioFile(event.target.files[0])
+    setAudioFile(e.target.files[0])
   }
 
   async function handleSubmit(e) {
     e.preventDefault()
-    console.log(audioFile)
-    const formData = new FormData()
-    formData.append('audioFile', audioFile)
-
     try {
-      const resp = await axios.get('/api/whisper', formData)
-      console.log(resp.status)
+      const reader = new FileReader()
+      reader.readAsDataURL(audioFile)
+      reader.onloadend = async () => {
+        const base64String = reader.result.replace(/^data:(.*;base64,)?/, '')
+        try {
+          const resp = await axios.post('/api/whisper', { audioFile: base64String })
+          console.log(resp.status)
+        } catch (err) {
+          console.error(err)
+        }
+      }
     } catch (err) {
       console.error(err)
     }
@@ -43,7 +50,7 @@ export default function WhisperPage() {
         <div>
           <label>Upload an audio file here!</label>
         </div>
-        <input type="file" value={audioFile} accept="audio/*" />
+        <input type="file" onChange={handleFileChange} />
         <button type="submit">Submit</button>
       </form>
       <button className="mt-4 rounded-sm outline" onClick={handleDemo}>
