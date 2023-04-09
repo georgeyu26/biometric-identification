9afb59ea75ea848414ad9257bd6e02df73bf25c0
Simeng He <28720612+simenghe@users.noreply.github.com>
2023-04-09 12:25:41 -0400

    Lazy

--- pages/api/face.js
+++ pages/api/face.js
@@ -3,10 +3,17 @@ import axios from 'axios'
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req, res) => {
   try {
+    const credentials = {
+      private_key:
+        '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQClu3tuuznbuAn9\nODaGduoHQW9FkRQdq7FaEelAK/W2kaKVgxN4NqytMV1FJGoAJ7NOHg3D9Gy2o9od\ntWgx8LFccwHvaMQzUGgiGBlvTmp4LyvuXrUpeJxWD8im5CkhKxhhbnc1ZdWMXbrA\n6LHkRbBVX4yUcMxk34EkMwYSzveRRT2z+Jto3sejFncbBeTOLMCfp6n56LVQ8YaE\n23XfJpBeOFbC7omx79ZXselFY3DfyY7QvLosb6oQu7R6RN5KEEhGIYPpZuUMejAo\nfQc6YO8tTO584gNQwJW4HB07gnhBfNweSPKFSp8/AOTINNSb6yQszVMiqU2AXTGy\nUhzrNmhBAgMBAAECggEACqFuvrDQOoWA0IcHSlM82jfKhyQR7gBKGTyuYUEgmvQX\nAW+ds1eQdnCE6s815GongHKBkAjw4zs53a957kAh5Ev1OxHVifQVpeA1W3QkdK6T\n6w+Un8Mehg+vn3Lhp4C/6NoA2AI+8yi89qxGYt3MfIFrP0dH+Q10F2oVcOXpL95u\ncJZTE15DOEkhpXcmsnEjw/ybahD0E5EPxSgWijLAGDVAZXWHrWL7nBZ6OXN/gWyq\nmaGc9yLott4E6MeDPIwaJ3SUZyNhGI4o54/hyLLtNjMrieKx393PqGmgz2UeoK0b\nfwiHXCwcHE3irTvGaRe7h9IgthihNvH2qCPgqsb0lQKBgQDVYkES6q03e6WJdC4C\nDEWZN/ZYC2s9FByuLoCxEvsQ05+artlcrJiD/ha3TByA1Sb2zhdW07mzMbS9HF0U\n12voyX8aCGgkyf5sHTHphaqG7aI0NRFbw/XnikRPfS5KNsQ2Vw8ikPuhGAG9pGka\ne7IvLn7nNFMfpcU2uGF1BZBWNQKBgQDG1O+gI9x7tGImRAiMca4E2lsrrVZZaCMA\nm6pGO+pRy1cv1S334JBt03FPDpAIe0k1kNhak8Y3zVZcRZi9hfytlcVklSzWKBGP\n5uOwraaYKvA2hjinB1KK2DfbUoNf2olhVeSjQhemU9FfvL58+iMl5Cf+NahrDUPj\npWV0aeGbXQKBgBZcmZ014rkxyANDnZ6xUongB3zqlW5tMSQ/01S7PHxi8RLwx+6i\nV8ldU1waKdKGg/LRXjjHL2rMBrjaxBZvDOv0UdGUl/VjBbqmxJhIXbqOJLV76jnH\nq3l8T/RUlglI+/8F2JwyvWwAbA5HsIwJ8n5nTP1FyoBCiwxU/kGZN6sRAoGAG1H0\n3w8uY30Eo3djqVCRda8/KcA2hjBt5s581guzhaun/jWPjGi5z61m+1Ln7ZJrG8Yc\nRE3bxZ0cSa9yblAzyPI0Xj61G3QD/b7e+E7T/kKgIQ3OSP0TlWpiOmubC5eumsY/\nlUMmlr8bnMkFb4y7OaCg8igTTSpUcrouN5G44y0CgYAfdlz7f83rMfoNdN629Gf3\nOVk5Qo3lfb76MM17xa/xciysnkzF64UZZbHpQ4uulsOM5nxrhDw9c/xKIyLNZGk2\nwcagdU4vaI7jwt3BmgmK6y3PxXerHfR3VVsba1Rrq0zmBL+jK7UMRrLVy5HdnpLY\nn42M+5h0SZdebWZo4fCvRQ==\n-----END PRIVATE KEY-----\n',
+      client_email: 'geeseresearch@cs-492-382206.iam.gserviceaccount.com',
+    }
+
     const vision = require('@google-cloud/vision')
 
     const client = new vision.ImageAnnotatorClient({
-      keyFilename: './Visionkey.json',
+      projectId: 'cs-492-382206',
+      credentials,
     })
 
     const base64String = req.body.imageFile
--- pages/api/gcpstt.js
+++ pages/api/gcpstt.js
@@ -7,9 +7,12 @@ export default async (req, res) => {
 
     // Creates a client
     // eslint-disable-next-line prettier/prettier
-    const client = new SpeechClient({
-      keyFilename: './Audiokey.json',
-    })
+    const credentials = {
+      client_email: 'keefecs492@cs492-382023.iam.gserviceaccount.com',
+      private_key:
+        '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC9nHTXAxLnBfLK\nbR+7I0qasDOdjcmYLlML8hUYxPM0UZkuXwuzSVxLy7QgaB2RqIfMBsY3H6eb61yt\n4EnieM5KRQyPQ3oQu5x7J+A4wA9d2j7UqPeREjzix9RPc8RQEOc5tE01QGuxhq8I\nldwMqUXvm3x5Rsx4RGcxM2ZoWzPGCaH3/UTFxijUF2ok4tOch5yxgRCeCQe0CLR1\n3z0M1iJg20NfU3d/aQPgGKY9e0Pe299TuXlofi0LpAi41gOTkLyz5SRaUUsBsZri\n/NudokQ3OxEmUCo+OOANf4kvHqhYaJ5bfgeZNoiW1IbCLhjL2kqgRImVBMGEUB9g\n9jQa+fELAgMBAAECggEAXhtcBicZZ54VLZDRkeDnMzVGuOjZaHV9dE1XWS7PAmWJ\nYHF97iL38wqMQRVLr7J0i7fFAzDr0EAVhJrwML4CrOvuWuCDofBrrthQufng5pTe\nYE/oZ5EktO23GyP7CD4LdSFevJJXoQmWr+0pBQOymUSTl+GitAJHMS0wo5S4co4T\nrfngbWkUq8NO5M2XzdiEWwalyZ2j9iF1V6Vux7j1Y6iSpMx10RFq52zYUPU6JC1Z\nDCWJwULBOvodL4q4mtZE33YeLKkt0hHFKbPanSWy9nVxdzwRqVTL5hL09+RyvlQx\ngP0eXVK4pMsDS2u21TgaacUN3GK/MMD25B9+G156cQKBgQDyOH6xSiQaTa31NMvT\n47eKjV3wvKOZefNj+ardScDVtE95HaA5OCbEkEYVblDrRRODhu/VyVlc1a+c/kD3\ntA8GZfVnScYTRdUCRmc/l9Zbxo6xWSqiRUrQ6DZsUIQypEMBXcUWNvwn8nMzDW7j\nOShXa78Eh5EAE9ZyFf94OiLfiQKBgQDIZcyVjPOl++SxqDCmzoZO214bTkGJfeAd\nX8NF9jIOmG39i4YH69uwRNBXIvkNl7+Mw/3oOri3tFm2HVMlgKVcAHPekhJjRVZB\nNRKu4UyQdLMksoJO3JnODPuk7RLfJR5x1BdQXCrLXkwPfXEP7fPmHB6kFwKJHW0A\nIsRIbb8y8wKBgAq6s+n4hcbgf/8nmovFP4EUp/+c64ZF36EjEw2AUdllhkuwI0PF\nl/oHBgO3AkCZq7acyIzowVuaX5ANtyUBxdSbuZYcr3h1FG1oXvRzLlycW//BGmFb\nvmeokZiUrHZtzkYGOE+2mc7PL4koZCwOgRn2PNzfgG3Hcy1Uih9yO885AoGBAL2Y\nMnq7JDlBnBjI+16VvlQVD2/eFfsy8DpaQ7GJP8q3RDjBLEmKSAr4z/PgzqihjURs\nmWiBRgnnw4idLfEpSUlF1Foa8PKiwvxmbgqudXWmM2bgzJA3/fwIJHzhHx2nTKp8\n8qHA0WwfBu8Qy0s64aovqH2ul2OKR3n9ap7r1h0pAoGBAJ+OwiGFsL+gpjZC/1Td\n9UpMXswte4eVfGUO410SaY5VQSOcc+W78Sg2WHT1UE0tGNy+bsO+g5KbkGgGlBTl\nIYcsB/TGjHyhOCMWd0SGgIRAyUdIEGKWZVjf7Hkvozk1QbH9R5kSTopqON5lxltJ\nYRJpP+aqBC9NcKT6vNAazLCO\n-----END PRIVATE KEY-----\n',
+    }
+    const client = new SpeechClient({ projectId: 'cs492-382023', credentials })
     const base64String = req.body.audioFile
     const buffer = Buffer.from(base64String, 'base64')
 
